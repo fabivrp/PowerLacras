65472d9d21845bf5fda1092ecc287382540238cb
juannava99 <juannadresnava99@gmail.com>
2021-11-10 06:40:26 -0300

    tratando de subir

--- powerlacras´s archives/juego.cpp
+++ powerlacras´s archives/juego.cpp
@@ -4,7 +4,7 @@
 
 
 
-const int PRODUCCION_MINIMA = 0;
+
 
 /*Juego::Juego()
 {
@@ -43,16 +43,6 @@ int Juego :: devolver_pos_vector(string nombre_a_buscar) {
     return pos_vector_edificio;
 }
 
-//cosa para ubicaciones
-int Juego :: devolver_pos_vector2(string nombre_a_buscar) {
-    int pos_vector_ubicaciones;
-    for (int i = 0 ; i < this->vector_ubicaciones.tamanio();i++){
-        Ubicaciones *ubicacions = vector_ubicaciones.devolver_info(i);
-            if (ubicacions->devolver_nombre() == nombre_a_buscar ) pos_vector_ubicaciones = i;
-    }
-    return pos_vector_ubicaciones;
-}
-
 void Juego :: agregar_ubicaciones(){
         mapa->mostrar();
     for(int i = 0 ; i < this->vector_ubicaciones.tamanio() ; i++){
@@ -68,322 +58,61 @@ void Juego :: agregar_ubicaciones(){
     cout << endl;
         mapa->mostrar();
 }
-//////////////////////////////////////////////////////////////////////////////////////////
-int calcular_espacio_delantero(int espacio, int  largo_palabra){
-    
-    return (espacio - largo_palabra)/2;
-}
-/*////////////////////////////////////////////////////////////////
-
-void Juego :: mostrar_edificios(){
-    cout << BGND_DARK_AQUA_29<<"╔-------------------------------------------------------------------------------------------------------╗" << END_COLOR << endl
-         << BGND_DARK_AQUA_29<<"|          TIPO         |            MATERIALES  REQUERIDOS             |   UNIDADES    |  DISPONIBLES  |" << END_COLOR << endl
-         << BGND_DARK_AQUA_29<<"|           DE          |-----------------------------------------------|      EN       |     PARA SU   |" << END_COLOR << endl
-         << BGND_DARK_AQUA_29<<"|        EDIFICIO       |     PIEDRA    |     MADERA    |     METAL     |   ANDYPOLIS   |  CONSTRUCCION |" << END_COLOR << endl;
-
-    for (int i = 0; i < vector_edificios.tamanio(); i++){
-        Edificios* aux = vector_edificios.devolver_info(i);
-        cout << BGND_DARK_AQUA_29<<"|-------------------------------------------------------------------------------------------------------|" << END_COLOR <<endl;
-
-        int espacio_delantero = calcular_espacio_delantero(ESPACIO_TIPO_EDIFICIO, aux->devolver_nombre().length());
-        int espacio_trasero = ESPACIO_TIPO_EDIFICIO - espacio_delantero - aux->devolver_nombre().length();
-        string material = to_string(aux->devolver_cantidad_piedra());
-        
-        cout << BGND_BLACK_16<< setfill(' ');
-        cout << "|" << setw(espacio_delantero) << "" << aux->devolver_nombre() << setw(espacio_trasero) << "|";
-        
-        
-        espacio_delantero = calcular_espacio_delantero (ESPACIO_MATERIALES, material.length());
-        espacio_trasero = ESPACIO_MATERIALES - espacio_delantero - material.length();
-        cout << setw(espacio_delantero) << "" << material << setw(espacio_trasero) << "|";
-        
-        material = to_string(aux->devolver_cantidad_madera());
-        espacio_delantero = calcular_espacio_delantero (ESPACIO_MATERIALES, material.length());
-        espacio_trasero = ESPACIO_MATERIALES - espacio_delantero - material.length();
-        cout << setw(espacio_delantero) << "" << material << setw(espacio_trasero) << "|";
-        
-        material = to_string(aux->devolver_cantidad_metal());
-        espacio_delantero = calcular_espacio_delantero (ESPACIO_MATERIALES, material.length());
-        espacio_trasero = ESPACIO_MATERIALES - espacio_delantero - material.length();
-        cout << setw(espacio_delantero) << "" << material << setw(espacio_trasero) << "|";
-        
-        material = to_string(aux->devolver_construidos());
-        espacio_delantero = calcular_espacio_delantero(ESPACIO_UNIDADES_CONSTRUIDAS, material.length());
-        espacio_trasero = ESPACIO_UNIDADES_CONSTRUIDAS - espacio_delantero - material.length();
-        cout << setw(espacio_delantero) << "" << material << setw(espacio_trasero) << "|";
-        
-        material = to_string(aux->devolver_max_construccion()- aux -> devolver_construidos());
-        espacio_delantero = calcular_espacio_delantero(ESPACIO_UNIDADES_DISPONIBLES, material.length());
-        espacio_trasero = ESPACIO_UNIDADES_DISPONIBLES - espacio_delantero - material.length();
-        cout << setw(espacio_delantero) << "" << material << setw(espacio_trasero) << "|" << END_COLOR <<endl;
-        
-    }
-        
-      
-    
-    cout << BGND_DARK_AQUA_29 << "╚-------------------------------------------------------------------------------------------------------╝" << END_COLOR << endl;
-    cout << endl;
-    
-}
-*/////////////////////////////////////////////////////////
-
-/*string Juego::extraer_coordenadas(string edificio){
-    string coordenadas = " ";
-
-    for(int i = 0 ; i < this->vector_ubicaciones.tamanio() ; i++){
-        Ubicaciones *ubicacion = vector_ubicaciones.devolver_info(i);
-        
-        if (edificio == ubicacion->devolver_nombre()){
-            coordenadas = coordenadas + '(' + to_string(ubicacion->devolver_fil()) + ',' + to_string(ubicacion->devolver_col()) + ") ";
-        }
-    }
-
-    return coordenadas;
-
-}*/
-
-
-void Juego :: mostrar_materiales(){
-    cout << "\t\t\t       "<< BGND_DARK_AQUA_29 <<"╔---------------------------------------╗" << END_COLOR << endl
-         << "\t\t\t       "<< BGND_DARK_AQUA_29 <<"|          TIPO         |   UNIDADES    |" << END_COLOR << endl
-         << "\t\t\t       "<< BGND_DARK_AQUA_29 <<"|           DE          |     EN EL     |" << END_COLOR << endl
-         << "\t\t\t       "<< BGND_DARK_AQUA_29 <<"|        MATERIAL       |     STOCK     |" << END_COLOR << endl;
-
-    for (int i = 0; i < this->vector_materiales.tamanio(); i++){
-        cout << "\t\t\t       "<< BGND_DARK_AQUA_29 <<"|-----------------------|---------------|" << END_COLOR <<endl;
-        
-        Material* aux = this->vector_materiales.devolver_info(i);
-
-        int espacio_delantero = calcular_espacio_delantero(ESPACIO_TIPO_MATERIAL, aux->devolver_nombre().length());
-        int espacio_trasero = ESPACIO_TIPO_MATERIAL - espacio_delantero - aux->devolver_nombre().length();
-
-        cout << "\t\t\t       " << BGND_BLACK_16<< setfill(' ');
-        cout << "|" << setw(espacio_delantero) << "" << aux->devolver_nombre() << setw(espacio_trasero) << "|";
-        
-        string caracteristica = to_string(aux->devolver_stock());
-    
-        espacio_delantero = calcular_espacio_delantero(ESPACIO_UNIDADES_CONSTRUIDAS, caracteristica.length());
-        espacio_trasero = ESPACIO_UNIDADES_CONSTRUIDAS - espacio_delantero - caracteristica.length();
-        cout << setw(espacio_delantero) << "" << caracteristica << setw(espacio_trasero) << "|" << END_COLOR << endl;   
-     
-      
-    }
-    
-    cout << "\t\t\t       " << BGND_DARK_AQUA_29 << "╚---------------------------------------╝" << END_COLOR << endl;
-    cout << endl;
-}
-void Juego::recolectar_materiales(){
-
-    for (int i = 0; i < vector_edificios.tamanio(); i++){
-        Edificios* aux = vector_edificios.devolver_info(i);
-
-        if (aux->cuanto_produce() > PRODUCCION_MINIMA){
-            for (int j = 0; j < vector_materiales.tamanio(); j++){
-                Material* aux_material = vector_materiales.devolver_info(j);
-              
-                if (aux->tipo_produccion() == aux_material->devolver_nombre()){
-                    aux_material->agregar_al_stock(aux->cuanto_produce());    
-                }
-            }
-        }
-    }    
-}
-
-void Juego :: describir_contenido(char tipo_casilla){
-     
-     switch (tipo_casilla) {
-        case ASERRADERO:
-
-            cout << "La casilla es de tipo terreno y tiene un Aserradero construido " << endl;
-
-            break;
-        case ESCUELA:
-
-            cout << "La casilla es de tipo terreno y tiene una Escuela construida " << endl;
-
-            break;
-        case MINA:
-
-            cout << "La casilla es de tipo terreno y tiene una Mina construida " << endl;
-
-            break;
-        case FABRICA:
-
-            cout << "La casilla es de tipo terreno y tiene una Fábrica construida " << endl;
-
-            break;
-        case OBELISCO:
-    
-            cout << "La casilla es de tipo terreno y tiene un Obelisco construido " << endl;
-    
-            break;
-        case PLANTA_ELECTRICA:
-           
-            cout << "La casilla es de tipo terreno y tiene una Planta eléctrica construida " << endl;
-
-            break;
-        case TERRENO:
-           
-            cout << "La casilla es de tipo terreno y está disponible para construcción " << endl;
-        
-            break;
-        case LAGO:
-           
-            cout << "La casilla es de tipo lago, no puedes construir sobre un cuerpo de agua " << endl;
-           
-           break;
-        case CAMINO:
-           
-            cout << "La casilla es de tipo lago, no puedes construir sobre un cuerpo de agua " << endl;
-           
-           break;
-        case METAL:
-           
-            cout << "La casilla es de tipo camino y tine material de tipo metal sobre ella " << endl;
-           
-           break;
-        case PIEDRA:
-            
-            cout << "La casilla es de tipo camino y tine material de tipo piedra sobre ella " << endl;
-           
-           break;
-        case MADERA:
-           
-            cout << "La casilla es de tipo camino y tine material de tipo madera sobre ella " << endl;
-           
-           break;
-    }
-}
-
-void Juego :: consultar_coordenada(){
-    
-    int fila, columna;
-    
-    cout << "Igrese la fila de la coordenada que desea consultar: ";
-    cin >> fila; 
-    
-    cout << "Igrese la columna de la coordenada que desea consultar: ";
-    cin >> columna; 
-
-    describir_contenido(mapa->consultar_tipo(fila,columna));
-}
-
-
-int Juego :: casillas_disponibles(){
-    int casillas_disponibles = 0;
-    for(int i = 0 ; i < mapa->devolver_filas() ; i++){
-        for(int j = 0; j < mapa->devolver_columnas() ; j++){
-            
-            if (mapa->consultar_tipo(i,j) == CAMINO){
-                casillas_disponibles++;
-            }
-        }       
-    }
-    return casillas_disponibles;
-}
-
-int Juego :: numero_aleatorio(int limite_inferior, int limite_superior){
-    return(limite_inferior + rand() % (limite_superior + 1 - limite_inferior)) ;
-}
-
-bool Juego :: casilla_disponible(Coordenada coordenada){
-   
-    return this->mapa->consultar_tipo(coordenada.fila, coordenada.columna) == CAMINO;
-
-}
-
-void Juego :: asignar_coordenada(Coordenada& coordenada){
-        
-    while (!casilla_disponible(coordenada)){ 
-        coordenada.fila = numero_aleatorio(MIN_FILAS, (mapa->devolver_filas()-1));
-        coordenada.columna = numero_aleatorio(MIN_COLUMNAS, (mapa->devolver_columnas()-1));    
-    }
-}
-
-void Juego :: colocar_material(int cantidad_material, char tipo){
-    Coordenada coordenada;
-    coordenada.fila = 0; 
-    coordenada.columna = 0; 
-
-    if (cantidad_material > 0){
-        
-        for (int i = 0; i < cantidad_material; i++){
-            
-            asignar_coordenada(coordenada);
-            Casillerot* casilla = new Casillerot(tipo);
-
-            mapa->actualizar_mapa(casilla, coordenada.fila, coordenada.columna);
-        }
-    }
-    
-
-}
-void Juego ::ejecutar_lluvia_materiales(){
-    srand((unsigned)time(NULL));
-    
-    int cantidad_madera, cantidad_metal, cantidad_piedra, total_materiales;
-
-    cantidad_madera = numero_aleatorio(MIN_MADERA, MAX_MADERA);
-    cantidad_piedra = numero_aleatorio(MIN_PIEDRA, MAX_PIEDRA);
-    cantidad_metal = numero_aleatorio(MIN_METAL, MAX_METAL);
-    
-    total_materiales = cantidad_madera + cantidad_metal + cantidad_piedra;
-
-    if (total_materiales <= casillas_disponibles()){ 
-        colocar_material(cantidad_madera, MADERA);
-        colocar_material(cantidad_piedra, PIEDRA);
-        colocar_material(cantidad_metal, METAL);
-    }
-    else cout << "No hay suficiente espacio para hacer la lluvia de materiales" << endl;
-}
-
-/*
-*/
 
 int Juego :: procesar_opcion(){
      string opcion;
-        //Opciones opciones(&this->vector_edificios,&this->vector_ubicaciones,&this->vector_materiales,this->mapa);
-        Construir_edificio optt(&this->vector_edificios,&this->vector_ubicaciones,&this->vector_materiales,this->mapa);
-        Listar_edificios_construidos xqw(&this->vector_edificios,&this->vector_ubicaciones,&this->vector_materiales,this->mapa);
-        Listar_edificios xqw1(&this->vector_edificios,&this->vector_ubicaciones,&this->vector_materiales,this->mapa);
-        Listar_materiales xqw2(&this->vector_edificios,&this->vector_ubicaciones,&this->vector_materiales,this->mapa);
+        Construir_edificio op1(&this->vector_edificios,&this->vector_ubicaciones,&this->vector_materiales,this->mapa);
+        Listar_edificios_construidos op2(&this->vector_edificios,&this->vector_ubicaciones,&this->vector_materiales,this->mapa);
+        Listar_edificios op3(&this->vector_edificios,&this->vector_ubicaciones,&this->vector_materiales,this->mapa);
+        Listar_materiales op7(&this->vector_edificios,&this->vector_ubicaciones,&this->vector_materiales,this->mapa);
+        Consultar_coordenada op6(&this->vector_edificios,&this->vector_ubicaciones,&this->vector_materiales,this->mapa);
+        Recolectar_recursos op8(&this->vector_edificios,&this->vector_ubicaciones,&this->vector_materiales,this->mapa);
+        Lluvia_recursos op9(&this->vector_edificios,&this->vector_ubicaciones,&this->vector_materiales,this->mapa);
         cout << "Ingresar una de las siguientes opciones:"<< endl;
         getline(cin,opcion);
         switch (stoi(opcion)) {
         case 1:
         cout << "entre aqui  1" << endl;
-        optt.accion();
+        op1.accion();
         this->mapa->mostrar();
             break;
         case 2:
             cout << "entre aqui  2" << endl;
-            xqw.accion();
+            op2.accion();
             break;
         case 3:
         cout << "entre aqui  3" << endl;
-            xqw1.accion();
+            op3.accion();
             
             break;
         case 4:
         cout << "entre aqui  4" << endl;
-            xqw2.accion();
+            //xqw2.accion();
             
             break;
         case 5:
+        mapa->mostrar();
         cout << "entre aqui  5" << endl;
         	
 			break;
         case 6:
+
         cout << "entre aqui  6" << endl;
+            op6.accion();
 			break;
         case 7:
         cout << "entre aqui  7" << endl;
+            op7.accion();
 			break;
         case 8:
         cout << "entre aqui  8" << endl;
+            op8.accion();
+        
 			break;
         case 9:
         cout << "entre aqui  9" << endl;
+            op9.accion();
+
 			break;
         case 10:
         cout << "entre aqui  10" << endl;
